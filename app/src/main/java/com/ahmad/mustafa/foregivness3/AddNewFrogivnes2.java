999b764e367d631a3a99202ba6688125a3ef4882
Ahmad mustafa <ahmadmust8>
2016-12-16 19:18:52 +0300

    first commit & Add all project files

---
+++ app/src/main/java/com/ahmad/mustafa/foregivness3/AddNewFrogivnes2.java
@@ -0,0 +1,15 @@
+package com.ahmad.mustafa.foregivness3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewFrogivnes2 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_frogivnes2);
+    }
+}
