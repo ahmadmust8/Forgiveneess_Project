c12cf616da16d246dde6041e1aff17623b39a50e
Ahmad mustafa <ahmadmust8>
2016-12-31 19:10:52 +0300

    add + icon & fix some problems in class

--- app/src/main/java/com/ahmad/mustafa/foregivness3/AddNewFrogivnes2.java
+++ app/src/main/java/com/ahmad/mustafa/foregivness3/AddNewFrogivnes2.java
@@ -1,15 +1,69 @@
 package com.ahmad.mustafa.foregivness3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddNewFrogivnes2 extends AppCompatActivity
 {
+
+    // all types of forgivness buttons
+    private Button forgivnes1;
+    private Button forgivnes2;
+    private Button forgivnes3;
+    private Button forgivnes4;
+    private Button newforgivnes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_frogivnes2);
+
+        // initials the buttons
+        forgivnes1 = (Button)findViewById(R.id.forgivnes_1);
+        forgivnes2 = (Button)findViewById(R.id.forgivnes_2);
+        forgivnes3 = (Button)findViewById(R.id.forgivnes_3);
+        forgivnes4 = (Button)findViewById(R.id.forgivnes_4);
+
+    }
+
+    // on any button of forgivnees is  clicked & choose which button is clicked
+    // & put it's name to Intent object & start
+    public void onClickForgivnessType(View view)
+    {
+        Intent intent = new Intent(getApplicationContext() , AstegfarActivity.class);
+        int id = view.getId();
+        switch (id)
+        {
+            case R.id.forgivnes_1:
+                intent.putExtra( "type" , forgivnes1.getText().toString());
+                break;
+
+            case R.id.forgivnes_2:
+                intent.putExtra( "type" , forgivnes2.getText().toString());
+                break;
+
+            case R.id.forgivnes_3:
+                intent.putExtra( "type" , forgivnes3.getText().toString());
+                break;
+
+            case R.id.forgivnes_4:
+                intent.putExtra("type" , forgivnes4.getText().toString());
+                break;
+
+            default:
+
+        }
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        finish();
     }
 }
--- app/src/main/java/com/ahmad/mustafa/foregivness3/AstegfarActivity.java
+++ app/src/main/java/com/ahmad/mustafa/foregivness3/AstegfarActivity.java
@@ -1,5 +1,6 @@
 package com.ahmad.mustafa.foregivness3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,24 +18,43 @@ public class AstegfarActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_astegfar);
+
+        //extract Intent data from Bundle object
+        Bundle bundle = getIntent().getExtras();
+        String typy = "";
+
+        // to avoid breakdown
+        if (bundle != null)
+        {
+            typy = bundle.getString("type");
+        }
+
         //create Button to compute the Forgiveness and implement onClick Method.
         forgiveness_counter =
                 (Button) findViewById(R.id.button_forgivenss_counter);
+        forgiveness_counter.setText(typy);
         forgiveness_counter.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                onCounterIncrease(view);
+                counterIncrease(view);
             }
         });
     }
 
-    public void onCounterIncrease(View view)
+    // on forgiveness_counter button clicked it's well be increase the counter 1
+    public void counterIncrease(View view)
     {
         counterInTextview = (TextView) findViewById(R.id.counter_textview);
         this.counter = this.counter + 1;
         counterInTextview.setText(this.counter+"");
     }
 
+    // to kill this Activity after pressed on back button
+    @Override
+    public void onBackPressed()
+    {
+        finish();
+    }
 }
